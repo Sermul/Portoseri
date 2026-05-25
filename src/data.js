7d080b53baff9d85a709491216b04be7350fd879
Sermul <sermullubis@gmail.com>
2026-05-26 01:35:39 +0700

    update

--- src/data.js
+++ src/data.js
@@ -158,19 +158,66 @@ import Proyek9 from "/assets/proyek/proyek9.png";
 import Proyek10 from "/assets/proyek/proyek10.png";
 import Proyek11 from "/assets/proyek/proyek11.png";
 export const listProyek = [
-  {
-    id: 1,
-    image: Proyek1,
-    title: "PetNest App",
-    subtitle: "A mobile application design for pet boarding and pet supplies shopping. Created using Figma,...",
-    fullDescription:"A mobile application design for pet boarding and pet supplies shopping. Created using Figma, focusing on user-friendly navigation, clean visuals, and seamless pet care booking experience.",
-    borderColor: "#3B82F6",
-    gradient: "linear-gradient(145deg, #3B82F6, #000)",
-    url: "https://www.figma.com/proto/Ey5HuBh9mcxOfqZcxOlFCg/PETNEST?node-id=116-2833&p=f&t=f71lDyKb54hQQwBu-0&scaling=scale-down&content-scaling=fixed&page-id=116%3A690",
-    dad: "100",
+ 
+ 
+ {
+  id: 1,
+  image: Proyek10,
+  title: "SEWEGER",
+  subtitle: "UI/UX design for a modern beverage brand website with a clean and aesthetic interface",
+  fullDescription: "SEWEGER is a beverage brand website design that I created with a modern and minimalist UI/UX approach. The website is designed to showcase drink products in a visually appealing way while maintaining a clean and easy-to-navigate user experience. I focused on creating responsive layouts, elegant visual compositions, and smooth user interaction to strengthen the brand identity and provide a comfortable browsing experience for users.",
+  borderColor: "#8b7b5e",
+  gradient: "linear-gradient(180deg, #c2a97d, #000)",
+  url: "https://www.figma.com/proto/l4gSYs2wTCoRwOl2YLi8KX/Suweger2?node-id=2008-662&p=f&t=uEmSXlucmv16saB4-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=2008%3A662",
+  date: "2026"
+},
+ {
+    id: 2,
+    image: Proyek6,
+    title: "Academic Achievement Reporting System – REST API",
+    subtitle: "A secure role-based backend API for managing student achievements, verification workflows...",
+    fullDescription:"A secure role-based backend API for managing student achievements, verification workflows, and academic reporting.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/Sermul/Prestasi_API",
+    dad: "600",
+  },
+    {
+    id: 3,
+    image: Proyek7,
+    title: "VenKampus",
+    subtitle: "Helping campus teams find the right vendors for their events",
+    fullDescription: "VenKampus is a web app I’m currently building to make it easier for students and campus organizations to connect with vendors for events. Instead of searching manually, users can explore different options like food, merchandise, and documentation in one place. For now, I’m focusing on crafting a clean and responsive frontend experience while gradually adding more features.",
+    borderColor: "#F59E0B",
+    gradient: "linear-gradient(180deg, #F59E0B, #000)",
+    url: "https://web-vendordanusan-ogor.vercel.app/",
+    dad: "2026"
+  },
+    {
+    id: 4,
+    image: Proyek8,
+    title: "Gymloc",
+    subtitle: "Helping people find the right gyms nearby with recommendations based on location, price, and facilities",
+    fullDescription: "GYMLOC is a web app I’m currently building to help users easily find gyms around them. Instead of searching manually, users can explore different gym options based on location, price, and available facilities in one place. For now, I’m focusing on creating a clean and responsive frontend while gradually adding more features like filtering and recommendations to enhance the user experience.",
+    borderColor: "#5aadbc",
+    gradient: "linear-gradient(180deg, #3c69c4, #000)",
+    url: "https://github.com/masterlearn22/GYMLOC.Reservavation_Gym",
+    dad: "2026"
   },
   {
-    id: 3,
+  id: 5,
+  image: Proyek11,
+  title: "Customer Journey Map Analysis",
+  subtitle: "UX research project focused on improving the usability and user experience of a school website.",
+  fullDescription:
+    "This project was created to analyze the user experience when searching for school information through a website. I developed a customer journey map to better understand user needs, identify the problems they encounter, and explore ways to improve the comfort, accessibility, and overall usability of the website.",
+  borderColor: "#c98b4d",
+  gradient: "linear-gradient(180deg, #e8b16a, #000)",
+  url: "https://www.figma.com/",
+  date: "2026"
+},
+  {
+    id: 7,
     image: Proyek3,
     title: "DigiPage App",
     subtitle: "A digital book reading mobile application built with Flutter. It allows users to explore, read, and...",
@@ -181,7 +228,7 @@ export const listProyek = [
     dad: "300",
   },
   {
-    id: 4,
+    id: 8,
     image: Proyek4,
     title: "Fashlook Web",
     subtitle: "A fashion inspiration website built using HTML, CSS, and JavaScript. It helps users find their...",
@@ -193,38 +240,15 @@ export const listProyek = [
   },
   {
     id: 6,
-    image: Proyek6,
-    title: "Academic Achievement Reporting System – REST API",
-    subtitle: "A secure role-based backend API for managing student achievements, verification workflows...",
-    fullDescription:"A secure role-based backend API for managing student achievements, verification workflows, and academic reporting.",
-    borderColor: "#10B981",
-    gradient: "linear-gradient(180deg, #10B981, #000)",
-    url: "https://github.com/Sermul/Prestasi_API",
-    dad: "600",
+    image: Proyek1,
+    title: "PetNest App",
+    subtitle: "A mobile application design for pet boarding and pet supplies shopping. Created using Figma,...",
+    fullDescription:"A mobile application design for pet boarding and pet supplies shopping. Created using Figma, focusing on user-friendly navigation, clean visuals, and seamless pet care booking experience.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://www.figma.com/proto/Ey5HuBh9mcxOfqZcxOlFCg/PETNEST?node-id=116-2833&p=f&t=f71lDyKb54hQQwBu-0&scaling=scale-down&content-scaling=fixed&page-id=116%3A690",
+    dad: "100",
   },
-  {
-    id: 7,
-    image: Proyek7,
-    title: "VenKampus",
-    subtitle: "Helping campus teams find the right vendors for their events",
-    fullDescription: "VenKampus is a web app I’m currently building to make it easier for students and campus organizations to connect with vendors for events. Instead of searching manually, users can explore different options like food, merchandise, and documentation in one place. For now, I’m focusing on crafting a clean and responsive frontend experience while gradually adding more features.",
-    borderColor: "#F59E0B",
-    gradient: "linear-gradient(180deg, #F59E0B, #000)",
-    url: "https://web-vendordanusan-ogor.vercel.app/",
-    dad: "2026"
-  },
-  {
-    id: 8,
-    image: Proyek8,
-    title: "Gymloc",
-    subtitle: "Helping people find the right gyms nearby with recommendations based on location, price, and facilities",
-    fullDescription: "GYMLOC is a web app I’m currently building to help users easily find gyms around them. Instead of searching manually, users can explore different gym options based on location, price, and available facilities in one place. For now, I’m focusing on creating a clean and responsive frontend while gradually adding more features like filtering and recommendations to enhance the user experience.",
-    borderColor: "#5aadbc",
-    gradient: "linear-gradient(180deg, #3c69c4, #000)",
-    url: "https://github.com/masterlearn22/GYMLOC.Reservavation_Gym",
-    dad: "2026"
-  },
-
   {
   id: 9,
   image: Proyek9,
@@ -235,29 +259,8 @@ export const listProyek = [
   gradient: "linear-gradient(180deg, #6ea8ff, #000)",
   url: "https://www.figma.com/proto/ywGO9R2mSVLZ1gM5ahBZKm/Untitled?node-id=1-427&t=NmxkXbgO5BfwLZ7r-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A427",
   dad: "2026"
-},
-{
-  id: 10,
-  image: Proyek10,
-  title: "SEWEGER",
-  subtitle: "UI/UX design for a modern beverage brand website with a clean and aesthetic interface",
-  fullDescription: "SEWEGER is a beverage brand website design that I created with a modern and minimalist UI/UX approach. The website is designed to showcase drink products in a visually appealing way while maintaining a clean and easy-to-navigate user experience. I focused on creating responsive layouts, elegant visual compositions, and smooth user interaction to strengthen the brand identity and provide a comfortable browsing experience for users.",
-  borderColor: "#8b7b5e",
-  gradient: "linear-gradient(180deg, #c2a97d, #000)",
-  url: "https://www.figma.com/proto/l4gSYs2wTCoRwOl2YLi8KX/Suweger2?node-id=2008-662&p=f&t=uEmSXlucmv16saB4-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=2008%3A662",
-  date: "2026"
-},
-
-{
-  id: 11,
-  image: Proyek11,
-  title: "Customer Journey Map Analysis",
-  subtitle: "UX research project focused on improving the usability and user experience of a school website.",
-  fullDescription:
-    "This project was created to analyze the user experience when searching for school information through a website. I developed a customer journey map to better understand user needs, identify the problems they encounter, and explore ways to improve the comfort, accessibility, and overall usability of the website.",
-  borderColor: "#c98b4d",
-  gradient: "linear-gradient(180deg, #e8b16a, #000)",
-  url: "https://www.figma.com/",
-  date: "2026"
 }
+
+
+
 ];
