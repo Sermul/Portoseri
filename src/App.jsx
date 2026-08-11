efef428f5d045420a59fc189770e857513157a19
Sermul <sermullubis@gmail.com>
2026-08-11 13:45:35 +0700

    update cv

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ function App() {
             />
             <div className="flex items-center sm:gap-4 gap-2">
               <a 
-                href="/assets/CV_Seri Muliani Lubis.pdf" 
+                href="/assets/CV_seri muliani lubis.pdf" 
                 download="CV_SERI.pdf" 
                 className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
               >
